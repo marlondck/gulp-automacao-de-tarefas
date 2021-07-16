f060fe222e0cd66d7eade5c62ba18a6214542753
marlondck <15050029+marlondck@users.noreply.github.com>
2021-07-15 21:53:19 -0400

    unificando arquivos js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+const htmlreplace = require('gulp-html-replace');
+
+gulp.task('default', ['copy'], () => {
+  // estas tasks são assincronas e não são dependentes entre si
+  gulp.start(['build-img', 'build-js', 'build-html']);
+});
 
 gulp.task('copy', ['clean'], () => {
   return gulp.src('src/**/*')
@@ -12,10 +19,24 @@ gulp.task('clean', () => {
     .pipe(clean());
 });
 
-gulp.task('build-img', ['copy'], () => {
+gulp.task('build-img', () => {
   gulp.src('dist/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 });
 
+gulp.task('build-js', () => {
+ gulp.src(['dist/js/jquery.js', 'dist/js/home.js', 'dist/js/produtos.js'])
+  .pipe(concat('all.js'))
+  .pipe(gulp.dest('dist/js'))
+});
+
+// html-replace utiliza atraves de comentario <!--build:js --> inserido no index.html para funcionar
+gulp.task('build-html', () => {
+  gulp.src('dist/**/*.html')
+    .pipe(htmlreplace({
+      js: 'js/all.js'
+    }))
+    .pipe(gulp.dest('dist'))
+});
 
